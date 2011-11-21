70b491522f8a69ee3d210e77a4cd6209f0e806e5
Eternal Night <eternal.n08@gmail.com>
2011-11-21 22:20:13 +0800

    Use original makefiles without cross-compiling prefix.

--- ucore/mk/compsv.mk
+++ ucore/mk/compsv.mk
@@ -1,10 +1,9 @@
-KPREFIX	:= x86_64-elf-
-CC		:= ${KPREFIX}gcc -m64 -ffreestanding \
+CC		:= gcc -m64 -ffreestanding \
 			-mcmodel=large -mno-red-zone \
 			-mno-mmx -mno-sse -mno-sse2 \
 			-mno-sse3 -mno-3dnow \
 			-fno-builtin -fno-builtin-function -nostdinc
 
-LD		:=  ${KPREFIX}ld -m $(shell ${KPREFIX}ld -V | grep elf_x86_64 2>/dev/null) -nostdlib
+LD		:=  ld -m $(shell ${KPREFIX}ld -V | grep elf_x86_64 2>/dev/null) -nostdlib
 
 include ${T_BASE}/mk/compopt.mk
--- ucore/mk/compu.mk
+++ ucore/mk/compu.mk
@@ -1,12 +1,10 @@
-KPREFIX	:= x86_64-elf-
-
-CC		:= ${KPREFIX}gcc -m64 -ffreestanding \
+CC		:= gcc -m64 -ffreestanding \
 			-mcmodel=small -mno-red-zone \
 			-mno-mmx -mno-sse -mno-sse2 \
 			-mno-sse3 -mno-3dnow \
 			-fno-builtin -fno-builtin-function -nostdinc
 
-LD		:=  ${KPREFIX}ld -m $(shell ${KPREFIX}ld -V | grep elf_x86_64 2>/dev/null) -nostdlib
-OBJCOPY :=  ${KPREFIX}objcopy
+LD		:=  $ld -m $(shell ${KPREFIX}ld -V | grep elf_x86_64 2>/dev/null) -nostdlib
+OBJCOPY :=  objcopy
 
 include ${T_BASE}/mk/compopt.mk
